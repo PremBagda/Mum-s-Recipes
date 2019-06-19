38678629f5eed8196715f4017d5667b349de083d
Prem Bagdawala <premrb91@gmail.com>
2019-06-19 15:09:47 +0800

    Add function to check validity of cuisine entered
    Add function that ensures only lowercase and uppercase alphabets are entered.

---
+++ public/js/mumsrecipes-script.js
@@ -0,0 +1,52 @@
+/*
+ * Checks the validity of the key entered for cuisine.
+ * return true cuisine consists of alphabets; otherwise false.
+ */
+function isCuisineValid(event) {
+    let code = event.key;
+    let charCode = code.charCodeAt(0);
+    // checks whether key entered is lowercase or uppercase alphabet.
+    return ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122));
+}
+
+/*
+ * Adds a cuisine specified by the user.
+ */
+function addCuisine() {
+
+    // Get the cuisine name that needs to be added.
+    var cuisineToAdd = document.getElementById("add-cuisine-form").value;
+
+    if (cuisineNameToAdd.length == 0) {
+        alert("Please enter cuisine.");
+        $("#modalCuisineForm").modal();
+    } else {
+        // Connecting to "receipes" database
+        const db_cuisines = firebase.firestore().collection("cuisine").doc("entire_list");
+
+        var cuisineArray;
+
+        // Get the names of all the cuisines in database
+        db_cuisines.get()
+            .then(doc => {
+                cuisineArray = doc.data().cuisineArray;
+                // Add new cuisine into array.
+                cuisineArray.push(cuisineNameToAdd);
+                cuisineArray.sort();
+                // cuisineArray.splice(0, 1);
+
+                // Overwritting the data in the database using the cuisineArray
+                db_cuisines.set({
+                    cuisineArray
+                })
+                    .then(function() {
+                        console.log("Document successfully written!");
+                    })
+                    .catch(function(error) {
+                        console.error("Error writing document: ", error);
+                    });
+
+            });
+        clearCuisineTextInput();
+    }
+}
